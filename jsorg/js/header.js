358ae510e9a17c10c96bd8773bce032f1f3bc20d
aleksandarilic <a.ilic@vegait.rs>
2022-06-14 02:48:34 +0200

    update header.js

--- jsorg/js/header.js
+++ jsorg/js/header.js
@@ -1,1 +1,14 @@
 // put event handlers for header links here
+$(document).ready(function() {
+    var $model = $("[rel='js-header']");
+    $("[rel='js-header']").on("click","[rel*='js-']", function(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        evt.stopImmediatePropagation();
+        var url = $(evt.target).attr("href");
+        $.ajax(url, {dataType: "text"})
+        .then(function(contents) {
+            $modal.html(contents).show();
+        });
+    });
+});
--- jsorg/notes/README.md
+++ jsorg/notes/README.md
@@ -1,3 +1,5 @@
 ### MODULES
 
 - python3 -m http.server 8003
+- rel="js-controls"
+- 
