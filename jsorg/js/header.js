02a0490fa50b4c7147cc7408b6432dd0e9691144
aleksandarilic <a.ilic@vegait.rs>
2022-06-14 03:10:28 +0200

    update header.js

--- jsorg/js/header.js
+++ jsorg/js/header.js
@@ -1,12 +1,16 @@
-// put event handlers for header links here
 $(document).ready(function() {
-    var $model = $("[rel='js-header']");
-    $("[rel='js-header']").on("click","[rel*='js-']", function(evt) {
+    var $modal = $("[rel='js-modal']");
+    //callback
+    $("[rel='js-controls']")
+    .on("click","[rel*='js-']", function(evt) {
+        console.log("url started");
         evt.preventDefault();
         evt.stopPropagation();
         evt.stopImmediatePropagation();
+
         var url = $(evt.target).attr("href");
-        $.ajax(url, {dataType: "text"})
+        console.log("url: " + url);
+        $.ajax(url, { dataType: "text"})
         .then(function(contents) {
             $modal.html(contents).show();
         });
