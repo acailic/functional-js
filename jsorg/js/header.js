dc97ba951ef42c3c3e0b7d8c90963b0cd939a46b
aleksandarilic <a.ilic@vegait.rs>
2022-06-14 08:10:44 +0200

    update header.js

--- jsorg/js/header.js
+++ jsorg/js/header.js
@@ -1,18 +1,14 @@
-$(document).ready(function() {
-    var $modal = $("[rel='js-modal']");
-    //callback
-    $("[rel='js-controls']")
-    .on("click","[rel*='js-']", function(evt) {
-        console.log("url started");
-        evt.preventDefault();
-        evt.stopPropagation();
-        evt.stopImmediatePropagation();
+var $modal = $("[rel=js-modal]");
 
-        var url = $(evt.target).attr("href");
-        console.log("url: " + url);
-        $.ajax(url, { dataType: "text"})
-        .then(function(contents) {
-            $modal.html(contents).show();
-        });
+$("[rel=js-header]").on("click","> [rel^=js]",function(evt){
+    evt.preventDefault();
+    evt.stopPropagation();
+    evt.stopImmediatePropagation();
+
+    var url = $(evt.target).attr("href");
+
+    $.ajax(url,{ dataType: "text" })
+    .then(function(contents){
+        $modal.html(contents).show();
     });
 });
